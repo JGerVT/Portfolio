c678b8097e942d9cba1f0ef97309253ca68a39ef
Jesse <115639935+JGerVT@users.noreply.github.com>
2023-12-19 13:54:30 -0500

    Finished About me page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/HomeSection/Home';
+import AboutMe from './Components/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App flex flex-col items-center">
       <Header/>
       <Home/>
-      {/* <div>TEST</div> */}
+      <AboutMe/>
+      <div className='flex w-full min-h-[600px] bg-[#101219]'>
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/AboutMe/AboutMe.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+import Placeholder from "../../Resources/placeholderProfile.png";
+import { MdOutlineEmail } from "react-icons/md";
+import { CiLinkedin } from "react-icons/ci";
+import { VscGithub } from "react-icons/vsc";
+import { FaMusic } from "react-icons/fa6";
+import { IoLogoGameControllerB } from "react-icons/io";
+import { FaBookOpen } from "react-icons/fa";
+
+export default function AboutMe() {
+    return (
+        <div className="p-28 flex space-x-[200px]">
+            <div id="LeftAboutSection">
+                <AboutMeProfilePicture />
+            </div>
+            <div id="RightAboutSection" className="text-left w-[680px] pt-6 space-y-[48px]">
+                <AboutMeInfo />
+                <AboutMeLinks />
+                <AboutMeInterests/>
+            </div>
+        </div>
+    );
+}
+
+
+function AboutMeProfilePicture() {
+    return (
+        <div className="relative overflow-hidden p-3">
+            <img
+                src={Placeholder}
+                alt="Profile"
+                className="w-[360px] h-[460px] object-cover object-center"
+            />
+
+            <div className="absolute top-0 left-0 h-[50px] w-[50px] border-l-2 border-t-2 border-[#3b61f8] " />
+            <div className="absolute bottom-0 right-0 h-[50px] w-[50px] border-r-2 border-b-2 border-[#3b61f8] " />
+        </div>
+    );
+}
+
+function AboutMeInfo() {
+    return (
+        <div id="AboutMeInfo">
+            <p className="text-lg text-[#5594F2]">Full Stack Developer</p>
+            <p className="text-2xl text-white font-semibold pb-4">About Me</p>
+            <p className="text-white text-base">
+                As a novice Full Stack Developer, I have hands-on experience in
+                creating responsive and dynamic user interfaces using React and
+                PyQt. My backend skills include working with Python and Java,
+                and I'm adept at managing data through SQL and JSON databases.
+            </p>
+        </div>
+    );
+}
+
+function AboutMeLinks() {
+    return (
+        <div id="AboutMeLinks">
+            <div className="space-y-4">
+                <AboutMeContent
+                    icon={<MdOutlineEmail size={"23px"} color="white" />}
+                    name="Email"
+                    value="email123@gmail.com"
+                />
+                <AboutMeContent
+                    icon={<CiLinkedin size={"23px"} color="white" />}
+                    name="LinkedIn"
+                    value="Jesse Germain"
+                />
+                <AboutMeContent
+                    icon={<VscGithub size={"23px"} color="white" />}
+                    name="GitHub"
+                    value="JGVT"
+                />
+            </div>
+        </div>
+    );
+
+    function AboutMeContent(props: {
+        icon: React.JSX.Element;
+        name: string;
+        value: string;
+    }) {
+        return (
+            <div className="flex">
+                <div className="flex w-[200px] items-center space-x-4">
+                    {props.icon}
+                    <p className="text-white">{props.name}</p>
+                </div>
+                <a className="text-[#5594F2]" href="https://">
+                    {props.value}
+                </a>
+            </div>
+        );
+    }
+}
+
+
+
+function AboutMeInterests() {
+    return (
+        <div id="AboutMeInterests">
+            <p className="text-lg text-white font-semibold pb-4">
+                My Interests
+            </p>
+            <div className="flex justify-between">
+                <AboutMeInterest
+                    icon={<FaMusic color="white" size={"23px"} />}
+                    Name="Music"
+                />
+                <AboutMeInterest
+                    icon={<IoLogoGameControllerB color="white" size={"23px"} />}
+                    Name="Gaming"
+                />
+                <AboutMeInterest
+                    icon={<FaBookOpen color="white" size={"23px"} />}
+                    Name="Learning"
+                />
+                <AboutMeInterest
+                    icon={<FaMusic color="white" size={"23px"} />}
+                    Name="Music"
+                />
+            </div>
+        </div>
+    );
+
+    function AboutMeInterest(props: { icon: React.JSX.Element; Name: string }) {
+        return (
+            <div className="flex space-x-4">
+                {props.icon}
+                <p className="text-white">{props.Name}</p>
+            </div>
+        );
+    }
+}
