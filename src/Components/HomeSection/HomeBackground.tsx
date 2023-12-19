3330f231916faa66fa03b733fac7790b8b7c76ac
Jesse <115639935+JGerVT@users.noreply.github.com>
2023-12-19 11:28:02 -0500

    Minor fixes for HomeBackground

--- src/Components/HomeSection/HomeBackground.tsx
+++ src/Components/HomeSection/HomeBackground.tsx
@@ -10,18 +10,15 @@ export function HomeBackground() {
         x: number;
         y: number;
     }>({ x: 0, y: 0 });
-    const [animateBackground, setAnimateBackground] = useState(true);
+    const animationScale = {Front: .009, Mid: .003, Back: .0008}
 
     useEffect(() => {
-        let centerOfContainer: { x: number; y: number } = { x: 0, y: 0 };
-
         if (ContainerRef.current) {
             const rect = ContainerRef.current.getBoundingClientRect();
 
             // Calculate the center coordinates
             const centerX = rect.left + rect.width / 2;
             const centerY = rect.top + rect.height / 2;
-            centerOfContainer = { x: centerX, y: centerY };
 
             const handleMouseMove = (e: MouseEvent) => {
                 const mouseX = e.clientX - centerX;
@@ -32,30 +29,25 @@ export function HomeBackground() {
                 }
             };
 
-            if(animateBackground){
-                // Attach the event listener to the document
-                document.addEventListener("mousemove", handleMouseMove);
-            }
-            else{
-                document.removeEventListener("mousemove", handleMouseMove);
-            }
+            // Attach the event listener to the document
+            document.addEventListener("mousemove", handleMouseMove);
 
             return () => {
                 document.removeEventListener("mousemove", handleMouseMove);
             };
         }
-    }, [animateBackground]);
+    }, []);
     
     return (
         <div
             ref={ContainerRef}
             id="Background"
-            className="relative flex z-0 pointer-events-none select-none pointer-events-auto h-full overflow-hidden"
+            className="relative flex z-0 pointer-events-none select-none h-full overflow-hidden"
         >
             <div
                 id="Back-IMG"
                 className={`absolute`}
-                style={{transform: `translate(${mousePosOffset.x * .0008}px, ${mousePosOffset.y * .0008}px)`}}
+                style={{transform: `translate(${mousePosOffset.x * animationScale.Back}px, ${mousePosOffset.y * animationScale.Back}px)`}}
             >
                 <img
                     src={Back}
@@ -64,7 +56,7 @@ export function HomeBackground() {
                 />
             </div>
             <div
-                style={{transform: `translate(${mousePosOffset.x * .003}px, ${mousePosOffset.y * .003}px)`}}>
+                style={{transform: `translate(${mousePosOffset.x * animationScale.Mid}px, ${mousePosOffset.y * animationScale.Mid}px)`}}>
                 <img
                     src={Mid}
                     alt=""
@@ -72,7 +64,7 @@ export function HomeBackground() {
                 />
             </div>
             <div
-                style={{transform: `translate(${mousePosOffset.x * .009}px, ${mousePosOffset.y * .009}px)`}}>
+                style={{transform: `translate(${mousePosOffset.x * animationScale.Front}px, ${mousePosOffset.y * animationScale.Front}px)`}}>
                 <img
                     src={Front}
                     alt=""
