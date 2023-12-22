2c8c00d486f7b0c29172a74f349d138a064b9c2e
Jesse <115639935+JGerVT@users.noreply.github.com>
2023-12-22 11:42:32 -0500

    Minor fixes to styling of footer

--- src/Pages/Footer/Footer.tsx
+++ src/Pages/Footer/Footer.tsx
@@ -3,35 +3,36 @@ import ParentSection from "../../Components/ParentSection";
 import Logo from "../../Resources/Logo.svg";
 import { FaGithub, FaLinkedin } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
+import { SmoothScrollToElement } from "../../Utility/utility";
 
 export default function Footer() {
     return (
         <ParentSection sectionID="Footer" paddingY="70px" backgroundColor="black">
             <div className="flex flex-1">
                 <div className="flex flex-1 h-[250px] justify-between">
-                    <div className="flex items-start text-white space-x-4 cursor-pointer border-r ml-6 pr-20 border-gray-700">
+                    <div className="flex items-start text-white space-x-4 border-r ml-6 pr-20 border-gray-700">
                         <img src={Logo} alt="Logo" className="w-[38px]" />
                         <div className="mt-1 pl-2">
                             <p className="text-[20px] leading-3">Jesse</p>
                             <p className="text-[20px] text-blue-500">Germain</p>
                         </div>
                     </div>
-                    <div className="flex items-start text-gray-300 space-x-4 cursor-pointer ml-6">
+                    <div className="flex items-start text-gray-300 space-x-4 ml-6">
                         <div className="mt-1 pl-2">
                             <p className="text-blue-500">Sitemap</p>
-                            <p className="">Home</p>
-                            <p className="">About</p>
-                            <p className="">Education</p>
-                            <p className="">Skills</p>
-                            <p className="">Past Works</p>
-                            <p className="">Contact</p>
+                            <p className="cursor-pointer hover:text-white" onClick={() => {SmoothScrollToElement("Home")}}>Home</p>
+                            <p className="cursor-pointer hover:text-white" onClick={() => {SmoothScrollToElement("About")}}>About</p>
+                            <p className="cursor-pointer hover:text-white" onClick={() => {SmoothScrollToElement("Education")}}>Education</p>
+                            <p className="cursor-pointer hover:text-white" onClick={() => {SmoothScrollToElement("Skills")}}>Skills</p>
+                            <p className="cursor-pointer hover:text-white" onClick={() => {SmoothScrollToElement("PastWorks")}}>Past Works</p>
+                            <p className="cursor-pointer hover:text-white" onClick={() => {SmoothScrollToElement("Contact")}}>Contact</p>
                         </div>
                     </div>
-                    <div className="flex items-start text-gray-300 space-x-4 cursor-pointer ml-6">
+                    <div className="flex items-start text-gray-300 space-x-4 ml-6">
                         <div className="mt-1 pl-2">
                             <p className="text-blue-500">Resources</p>
                             <a
-                                className=""
+                                className="hover:text-white"
                                 href="https://react-icons.github.io/react-icons/"
                             >
                                 React Icons
