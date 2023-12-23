d8f8c896c647a19de90e708f2e67c972bb75c9a4
Jesse <115639935+JGerVT@users.noreply.github.com>
2023-12-23 11:05:04 -0500

    Added functional scrolling in Projects section

--- src/Pages/PastWorks/PastWorks.tsx
+++ src/Pages/PastWorks/PastWorks.tsx
@@ -1,14 +1,106 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ParentSection from "../../Components/ParentSection";
 import ShinyOverlay from "../../Resources/shinyOverlay.png";
 import { BiSolidRightArrow } from "react-icons/bi";
 import { projectsData } from "./projectsData";
 
 export default function PastWorks() {
-    const [canScroll, setCanScroll] = useState<{
-        canScrollLeft: boolean;
-        canScrollRight: boolean;
-    }>({ canScrollLeft: false, canScrollRight: true });
+    const [canScrollRight, setCanScrollRight] = useState(false);
+    const [canScrollLeft, setCanScrollLeft] = useState(false);
+    const GameContainer = useRef<HTMLDivElement>(null);
+
+    function ScrollBoxRight(e: React.MouseEvent) {
+        if (GameContainer.current?.children) {
+            scrollToNearestItem(
+                GameContainer.current.getBoundingClientRect().width
+            );
+        }
+    }
+
+    function ScrollBoxLeft(e: React.MouseEvent) {
+        if (GameContainer.current?.children) {
+            scrollToNearestItem(
+                -GameContainer.current.getBoundingClientRect().width
+            );
+        }
+    }
+
+    function scrollToNearestItem(scrollPositionOffset: number) {
+        const scrollBox = GameContainer.current;
+
+        if (scrollBox) {
+            // Set the desired scroll position with the scrollPositionOffset
+            const scrollPos =
+                GameContainer.current?.scrollLeft! + scrollPositionOffset;
+            const items = Array.from(scrollBox.children);
+
+            let nearestItem:
+                | {
+                      offset: number;
+                      item: any;
+                  }
+                | undefined = { offset: 0, item: null };
+
+            // Loop through each item to find the closest item to the desired scroll position
+            items.forEach((item) => {
+                const itemActualPosX =
+                    GameContainer.current?.scrollLeft! +
+                    item.getBoundingClientRect().x -
+                    scrollBox.getBoundingClientRect().left;
+                const offset = Math.abs(scrollPos - itemActualPosX);
+
+                if (
+                    nearestItem?.item === null ||
+                    offset < nearestItem!.offset
+                ) {
+                    nearestItem = { offset: offset, item: item };
+                }
+            });
+
+            // Move to that nearest item
+
+            if (nearestItem.item === GameContainer.current.children[0]) {
+                GameContainer.current.scrollTo({ left: 0, behavior: "smooth" });
+            } else if (nearestItem?.item != null) {
+                (nearestItem.item as HTMLElement).scrollIntoView({
+                    behavior: "smooth",
+                    block: "nearest",
+                    inline: "start",
+                });
+            }
+        }
+    }
+
+    // verify that can scroll on x axis
+    useEffect(() => {
+        const scrollBox = GameContainer.current;
+
+        if (scrollBox) {
+            const handleScroll = () => {
+                // Check if the container can scroll in the X-axis
+                const canScroll = scrollBox.scrollWidth > scrollBox.clientWidth;
+                const isAtEnd =
+                    scrollBox.scrollLeft + scrollBox.clientWidth >=
+                    scrollBox.scrollWidth - 1;
+                const isAtStart = scrollBox.scrollLeft === 0;
+
+                setCanScrollRight(canScroll && !isAtEnd);
+                setCanScrollLeft(canScroll && !isAtStart);
+            };
+
+            // Add event listener for scroll events
+            scrollBox.addEventListener("scroll", handleScroll);
+
+            // Initial check
+            handleScroll();
+
+            // Cleanup: remove event listener when component unmounts
+            return () => {
+                scrollBox.removeEventListener("scroll", handleScroll);
+            };
+        }
+    }, []);
+
 
     return (
         <ParentSection
@@ -28,6 +120,7 @@ export default function PastWorks() {
                         <ScrollButtons />
                     </div>
                     <div
+                        ref={GameContainer}
                         id="ProjectsContainer"
                         className="w-full flex space-x-[47px] overflow-x-hidden overflow-y-hidden pb-4"
                     >
@@ -51,9 +144,10 @@ export default function PastWorks() {
         return (
             <div className="flex">
                 <button
+                    onClick={ScrollBoxLeft}
                     className="w-[25px] h-[25px] rounded-l flex justify-center items-center border border-[#3B61F8]"
                     style={{
-                        backgroundColor: canScroll?.canScrollLeft
+                        backgroundColor: canScrollLeft
                             ? "#3B61F8"
                             : "transparent",
                     }}
@@ -61,9 +155,10 @@ export default function PastWorks() {
                     <BiSolidRightArrow color="white" className="rotate-180" />
                 </button>
                 <button
+                    onClick={ScrollBoxRight}
                     className="w-[25px] h-[25px] rounded-r flex justify-center items-center border border-[#3B61F8]"
                     style={{
-                        backgroundColor: canScroll?.canScrollRight
+                        backgroundColor: canScrollRight
                             ? "#3B61F8"
                             : "transparent",
                     }}
--- src/Pages/PastWorks/projectsData.ts
+++ src/Pages/PastWorks/projectsData.ts
@@ -8,77 +8,82 @@ export interface ProjectData {
     projectType: string;
     projectName: string;
     projectText: string;
-    links: {name: string, link: string}[]
+    links: { name: string; link: string }[];
     screenshots: never[];
 }
 export const projectsData: ProjectData[] = [
     {
         projectScreenshot: PortfolioScreenshot,
-        projectType: "Website (React)",
+        projectType: "Website (React / TypeScript)",
         projectName: "Portfolio Website",
-        projectText: "This project is the project that you are currently looking at.",
-        links:[
+        projectText: "My portfolio website that you are currently viewing.",
+        links: [
             {
                 name: "Website",
-                link: "http://localhost:3000/"
+                link: "http://localhost:3000/",
             },
             {
                 name: "GitHub",
-                link: "https://github.com/JGerVT/"
+                link: "https://github.com/JGerVT/",
             },
         ],
-        screenshots: []
+        screenshots: [],
     },
     {
         projectScreenshot: HabitTracker,
         projectType: "Software (React / Electron)",
         projectName: "HabitTracker",
-        projectText: "Habit Tracker is an app that allows you to track progress for habits, while visually displaying that progress on a monthly heatmap per task.",
-        links:[
+        projectText:
+            "Habit Tracker is an app that allows you to track progress for habits, while visually displaying that progress on a monthly heatmap per task.",
+        links: [
             {
                 name: "GitHub",
-                link: "https://github.com/JGerVT/InspireCanvas"
-            }
+                link: "https://github.com/JGerVT/InspireCanvas",
+            },
         ],
-        screenshots: []
-    },
-    {
-        projectScreenshot: InspireCanvasScreenshot,
-        projectType: "Software (PyQt)",
-        projectName: "Inspire Canvas",
-        projectText: "Inspire Canvas is an application that enables users to arrange, resize, and reposition Images, Text, and Files on a two-dimensional canvas.",
-        links:[
-            {
-                name: "GitHub",
-                link: "https://github.com/JGerVT/InspireCanvas"
-            }
-        ],
-        screenshots: []
+        screenshots: [],
     },
     {
         projectScreenshot: VGMDB,
-        projectType: "Website (React / Node)",
+        projectType: "Website (React / TypeScript)",
         projectName: "VGDB",
-        projectText: "VGDB (Video Game Database) is a website that allows users to Organize, Rate, and Catalogue their favorite games.",
-        links:[
+        projectText:
+            "VGDB (Video Game Database) is a website that allows users to Organize, Rate, and Catalogue their favorite games.",
+        links: [
             {
                 name: "GitHub",
-                link: "https://github.com/JGerVT/InspireCanvas"
-            }
+                link: "https://github.com/JGerVT/InspireCanvas",
+            },
         ],
-        screenshots: []
+        screenshots: [],
     },
+    {
+        projectScreenshot: InspireCanvasScreenshot,
+        projectType: "Software (Python / PyQt)",
+        projectName: "Inspire Canvas",
+        projectText:
+            "Inspire Canvas is an application that enables users to arrange, resize, and reposition Images, Text, and Files on a two-dimensional canvas.",
+        links: [
+            {
+                name: "GitHub",
+                link: "https://github.com/JGerVT/InspireCanvas",
+            },
+        ],
+        screenshots: [],
+    },
+
     {
         projectScreenshot: VGMDB,
         projectType: "Website (React / Node)",
         projectName: "Realtor.com Clone",
-        projectText: "Realtor.com Clone is recreation of the realtor.com website.",
-        links:[
+        projectText:
+            "Realtor.com Clone is recreation of the realtor.com website.",
+        links: [
             {
                 name: "GitHub",
-                link: "https://github.com/JGerVT/InspireCanvas"
-            }
+                link: "https://github.com/JGerVT/InspireCanvas",
+            },
         ],
-        screenshots: []
+        screenshots: [],
     },
 ];
